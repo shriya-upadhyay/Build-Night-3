19e168c30e02241345f31b4ed6f4838c42eb92b9
shriya-upadhyay <shriyaup@usc.edu>
2024-11-18 17:28:19 -0800

    updated template page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -21,9 +21,6 @@ export default function Home() {
   const [sendAmount, setsendAmount] = useState('');
 
 
-  };
-
-
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
@@ -82,5 +79,5 @@ export default function Home() {
       </div>
   );
 
-}
+};
 
